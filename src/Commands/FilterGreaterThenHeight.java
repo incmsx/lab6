9abed2a9a974fe0261670254fccdb154f4e6c0ff
incmsx <153332794+incmsx@users.noreply.github.com>
2024-04-08 15:14:14 +0300

    FilterGreaterThenHeight

--- src/Commands/FilterGreaterThenHeight.java
+++ src/Commands/FilterGreaterThenHeight.java
@@ -1,12 +1,34 @@
 package Commands;
 
+import Fillers.PersonGenerator;
+import InputData.Person;
 import Interfaces.ICommand;
+import Managers.CollectionManager;
+import Managers.Parser;
+
+import java.util.Hashtable;
+import java.util.Map;
 
 //Receiver
 public class FilterGreaterThenHeight implements ICommand {
     @Override
     public void execute(String[] args)
     {
-
+        Hashtable<Long, Person> personTable = CollectionManager.getPersonCollection();
+        try
+        {
+            Integer height = Integer.valueOf(Parser.parseArgument(args));
+            for (Map.Entry<Long, Person> entry : personTable.entrySet())
+            {
+                if (entry.getValue().getHeight() > height)
+                {
+                    System.out.println(entry.getValue().toString());
+                }
+            }
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println("Height элемента введена неправильно. ");
+        }
     }
 }
